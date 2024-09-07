9f50a4d0eefb202a2ccae154d0528a6987c6b4e4
Smashbrick <smashbrick@yahoo.com>
2024-09-08 03:25:10 +0530

    First commit🚀

---
+++ app/dashboard/page.js
@@ -0,0 +1,5 @@
+function dashboard() {
+	return <div>Hello I am under the water</div>;
+}
+
+export default dashboard;
---
+++ components/Main.js
@@ -0,0 +1,6 @@
+function Main(props) {
+	const { Children } = props;
+	return <div>{Children}</div>;
+}
+
+export default Main;
